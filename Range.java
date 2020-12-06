2ec370741dcc924fa873898e58170598950645f9
adnanq2004 <aquayyum20@stuy.edu>
2020-12-06 16:51:34 -0500

    made range class

---
+++ Range.java
@@ -0,0 +1,33 @@
+import java.util.NoSuchElementException;
+
+public class Range implements IntegerSequence {
+
+	private int start, end, current;
+
+	public Range(int start, int end) {
+		this.start = start; 
+		this.end = end;
+		current = start;
+	}
+
+	public void reset() {
+		current = start;
+	}
+
+	public int length() {
+		return (end - start) +1;
+	}
+
+	public boolean hasNext() {
+		return (current < end);
+	}
+
+	public int next() {
+		int val = current;
+		if (hasNext()) {
+			current ++;
+		}
+		return val;
+	}
+
+}
