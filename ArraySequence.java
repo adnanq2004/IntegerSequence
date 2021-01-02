b676b826809a8e372e41ac2c826cb1d3585dac79
adnanq2004 <aquayyum20@stuy.edu>
2021-01-02 12:45:09 -0500

    fixed error with next() method

--- ArraySequence.java
+++ ArraySequence.java
@@ -6,12 +6,27 @@ public class ArraySequence implements IntegerSequence{
   private int[] data;
 
   public ArraySequence(int[] other) {
-    data = new int[other.length];
+    /*data = new int[other.length];
     for (int i = 0; i < other.length; i++) {
       data[i] = other[i];
-    }
-    currentIndex = 0;
+    }*/
+	data = new int[other.length];
+	for (int i = 0; i < other.length; i++) {
+		data[i] = other[i];
+	}
+    	currentIndex = 0;
   }
+
+  /*public ArraySequence(IntegerSequence otherseq){
+	data = new int[otherseq.length()];
+	otherseq.reset();
+	int i = 0;
+	while(otherseq.hasNext()) {
+		data[i] = otherseq.next();
+		i++;
+	}
+	otherseq.reset();
+  } */
 
   public void reset() {
     currentIndex = 0;
@@ -22,10 +37,11 @@ public class ArraySequence implements IntegerSequence{
   }
 
   public int next() {
-    if (!hasNext())
+    if (!hasNext()) {
 	    throw new NoSuchElementException("No Next Value");
-    currentIndex++;
-    return currentIndex -1;
+    	}
+	    currentIndex++;
+    return data[currentIndex -1];
 
 /*if (!hasNext()) throw new NoSuchElementException("No Next Value");
     int val = currentIndex;
